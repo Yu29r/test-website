da7c1af80992c07599a34fc74f8f6c52b327f340
Yu29r <123236487+Yu29r@users.noreply.github.com>
2023-02-14 15:03:16 +0300

    Add files via upload

---
+++ styles/index.js
@@ -0,0 +1,20 @@
+unction caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+case 1:
+return answer "alpha"
+break;
+case "2":
+return "beta";
+break;
+case 3:
+return "gamma";
+break;
+case 4:
+return "delta";
+break;
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
